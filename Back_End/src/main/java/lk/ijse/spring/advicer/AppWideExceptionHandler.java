7a871428531e35d2c5d01527babe9093cc8dd730
hansakam <mhansaka1@gmail.com>
2023-10-26 04:13:17 +0530

    create a response util and advicer packages

---
+++ Back_End/src/main/java/lk/ijse/spring/advicer/AppWideExceptionHandler.java
@@ -0,0 +1,8 @@
+/**
+ * @author :Hansaka Malshan
+ * created 10/26/2023---4:10 AM
+ */
+package lk.ijse.spring.advicer;
+
+public class AppWideExceptionHandler {
+}
---
+++ Back_End/src/main/java/lk/ijse/spring/util/ResPonseUtil.java
@@ -0,0 +1,8 @@
+/**
+ * @author :Hansaka Malshan
+ * created 10/26/2023---4:12 AM
+ */
+package lk.ijse.spring.util;
+
+public class ResPonseUtil {
+}
