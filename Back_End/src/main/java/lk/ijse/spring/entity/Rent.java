455192aaa7b705d5e0bfa34f1287c1f8f7f21a2e
hansakam <mhansaka1@gmail.com>
2023-10-28 18:16:21 +0530

    fill the rent entity

--- Back_End/src/main/java/lk/ijse/spring/entity/Rent.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Rent.java
@@ -4,5 +4,29 @@
  */
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Rent {
+    @Id
+    private String rentId;
+    private String date;
+    private String pickupDate;
+    private String returnDate;
+    private String pickUpVenue;
+    private String returnVenue;
+    private String lossDamageWaiver;
+    private String bankSlip;
+    private String status;
+
 }
