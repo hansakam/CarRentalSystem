54d0d3a0a9cbd9f06f4dfd6748cf0b8686b6f88b
hansakam <mhansaka1@gmail.com>
2023-10-28 21:03:31 +0530

    fill CustomerserviceImpl and content added

--- Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -1,6 +1,21 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerService{
+
+    void saveCustomer(CustomerDTO dto);
+
+    void deleteCustomer(String id);
+
+    List<CustomerDTO> getAllCustomer();
+
+    CustomerDTO findCustomer(String id);
+
+    void updateCustomer(CustomerDTO dto);
+
+
 }
--- Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -4,13 +4,49 @@
  */
 package lk.ijse.spring.service.impl;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
+    CustomerRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
+    public void saveCustomer(CustomerDTO dto) {
+        if (repo.existsById(dto.getCustomerId())){
+            throw new RuntimeException(dto.getCustomerId()+"Customer Is already Added !");
+        }
+
+        repo.save( mapper.map(dto, Customer.class));
+
+    }
+
+    public void deleteCustomer(String id) {
+
+    }
+
+    public List<CustomerDTO> getAllCustomer() {
+        return null;
+    }
+
+    public CustomerDTO findCustomer(String id) {
+        return null;
+    }
+
+    public void updateCustomer(CustomerDTO dto) {
+
+    }
 }
