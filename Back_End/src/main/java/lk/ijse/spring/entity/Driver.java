e920e9eb6cb0df018188f25edc5206dc7f76e241
hansakam <mhansaka1@gmail.com>
2023-10-28 18:08:38 +0530

    fill driver entity

--- Back_End/src/main/java/lk/ijse/spring/entity/Driver.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Driver.java
@@ -4,5 +4,29 @@
  */
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Driver {
+    @Id
+    private String licenceNo;
+    private String name;
+    private String contactNo;
+    private String nicNo;
+    private String username;
+    private String password;
+    private String email;
+    private String availability;
+
+
 }
