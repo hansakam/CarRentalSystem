caad2f2e3cb9554204b1c74da5a53fd29fbaf9a9
hansakam <mhansaka1@gmail.com>
2023-10-28 18:00:43 +0530

    fill the car data in entity

--- Back_End/src/main/java/lk/ijse/spring/entity/Car.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Car.java
@@ -4,5 +4,37 @@
  */
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Car {
+    @Id
+    private String reggistrationNo;
+    private String brand;
+    private String type;
+    private int NoOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+    private double dailyRate;
+    private double monthlyRate;
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double freeMileage;
+    private String status;
+    private String frontViewImage;
+    private String backView;
+    private String sideView;
+    private String internalView;
+
 }
