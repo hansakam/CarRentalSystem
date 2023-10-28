1f2803e00545c4ec09710c5ff947ab7c205c6242
hansakam <mhansaka1@gmail.com>
2023-10-28 18:28:25 +0530

    Careate a ca DTO class

---
+++ Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -0,0 +1,37 @@
+/**
+ * @author :Hansaka Malshan
+ * created 10/28/2023---6:26 PM
+ */
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CarDTO {
+    private String registrationNO;
+    private String brand;
+    private String type;
+    private int NoOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+
+    private String frontViewImage;
+    private String backView;
+    private String sideView;
+    private String internalView;
+
+    private double dailyRate;
+    private double monthlyRate;
+
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double freeMileage;
+    private String status;
+}
--- Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
@@ -4,6 +4,15 @@
  */
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class DriverDTO {
 
     private String licenceNo;
