b2873ec0d405a2bbb4bd0dfba5cadb934e2d4f41
hansakam <mhansaka1@gmail.com>
2023-10-28 20:42:13 +0530

    change some package

--- Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -2,7 +2,7 @@
  * @author :Hansaka Malshan
  * created 10/28/2023---7:25 PM
  */
-package lk.ijse.spring.repo.impl;
+package lk.ijse.spring.service.impl;
 
 public class AdminServiceImpl {
 }
--- Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -2,7 +2,7 @@
  * @author :Hansaka Malshan
  * created 10/28/2023---7:21 PM
  */
-package lk.ijse.spring.repo.impl;
+package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.service.CustomerService;
 import org.springframework.stereotype.Service;
