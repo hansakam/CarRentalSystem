f3e5ef3ec2e235f90c9a3fc88d98dbe8bbd57494
hansakam <mhansaka1@gmail.com>
2023-10-28 19:24:08 +0530

    create impl,service package with customerService and impl

---
+++ Back_End/src/main/java/lk/ijse/spring/repo/impl/CustomerServiceImpl.java
@@ -0,0 +1,16 @@
+/**
+ * @author :Hansaka Malshan
+ * created 10/28/2023---7:21 PM
+ */
+package lk.ijse.spring.repo.impl;
+
+import lk.ijse.spring.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService {
+
+}
---
+++ Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -0,0 +1,6 @@
+package lk.ijse.spring.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerService{
+}
