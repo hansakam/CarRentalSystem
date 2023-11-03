39665b275e5024784ccdeed3818e13ff49339569
hansakam <mhansaka1@gmail.com>
2023-11-03 06:27:32 +0530

    fill the customerDTO

--- Back_End/src/main/java/lk/ijse/spring/dto/RentDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/RentDTO.java
@@ -4,5 +4,27 @@
  */
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class RentDTO {
+    private String rentId;
+    private String date;
+    private String pickupDate;
+    private String returnDate;
+    private String pickUpVenue;
+    private String returnVenue;
+    //private String lossDamageWaiver;
+    private String bankSlip;
+    private String status;
+
+    private CarDTO car;
+    private CustomerDTO customer;
+    private DriverDTO driver;
 }
